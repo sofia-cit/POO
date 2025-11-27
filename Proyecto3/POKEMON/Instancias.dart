80431e92c67cf85224118e5e99cdd941c70334ff
Cit <sbernaldelao@gmail.com>
2025-11-26 18:42:20 -0600

    Actualizacion de Instancias Falta ataques y pokemones

--- Proyecto3/POKEMON/Instancias.dart
+++ Proyecto3/POKEMON/Instancias.dart
@@ -3,20 +3,111 @@ import '../avance/base.dart';
 
 // DEFINICION DE ATAQUES
 
-//fuego
-final Ataque lanzallamas = Ataque("Fuego", "Lanzallamas", 90);
-final Ataque ascuas = Ataque("Fuego", "Ascuas", 40);
-final Ataque giroFuego = Ataque("Fuego", "Giro Fuego", 35);
+//Steel
+final Ataque colaFerrea = Ataque("Steel","Cola Ferrera",100);
+final Ataque garraMetal= Ataque("Steel","Garra Metal",50);
+final Ataque cuerpoPesado= Ataque("Steel","Cuerpo Pesado",80);
+final Ataque cabezaDeHierro= Ataque("Steel","Cabeza de Hierro",80);
 
-//agua
+//Dark
+final Ataque finta= Ataque("Dark","Finta",60);
+final Ataque pulsoUmbrio= Ataque("Dark","Pulso Umbrio",80);
+final Ataque juegoSucio= Ataque("Dark","Juego Sucio",95);
+final Ataque mordisco= Ataque("Dark","Mordisco",60);
+final Ataque tajoUmbrio= Ataque("Dark","Tajo Umbrio",70);
+
+
+//Dragon
+final Ataque colaDragon= Ataque("Dragon","Cola Dragon",80);
+final Ataque garraDragon= Ataque("Dragon","Garra Dragon",80);
+final Ataque enfado=Ataque("Dragon","Enfado",120);
+final Ataque vastoImpacto= Ataque("Dragon","Vasto Impacto",80);
+final Ataque danzaDragon= Ataque("Dragon","Danza Dragon",20);
+
+
+//Ghost
+final Ataque sombraVil= Ataque("Ghost","Sombra Vil",90);
+final Ataque bolaSombra= Ataque("Ghost","Bola Sombra",80);
+final Ataque puasToxicas= Ataque("Ghost","Púas Tóxicas",20);//mayor probabilidad de envenenar 
+final Ataque escudoReal= Ataque("Ghost","Escudo Real",10);
+final Ataque fuegoFatuo= Ataque("Ghost","Fuego Fatuo",50);//mayor probabilidad de estado quemado
+final Ataque infortunio= Ataque("Ghost","Infortunio",65);
+final Ataque RayoConfuso= Ataque("Ghost","Rayo Confuso",40);//mayor probabilidad de estado confuso
+
+//Rock
+final Ataque rocaAfilada= Ataque("Rock","Roca Afilada",100);
+final Ataque triturar= Ataque("Rock","Triturar",80);
+final Ataque trampaRocas= Ataque("Rock","Trampa Rocas",15);//mayor probabilidad estado
+final Ataque explosion= Ataque("Rock","Explosión",150);
+final Ataque avalancha= Ataque("Rock","Avalancha",75);
+final Ataque romperrocas= Ataque("Rock","Romperrocas",40);
+final Ataque cuernoTaladro= Ataque("Rock","Cuerno Taladros",50);
+final Ataque cabezazoRoca= Ataque("Rock","Cabezazo Roca",150);
+
+//Bug
+final Ataque IdaYVuelta= Ataque("Bug","Ida y Vuelta",70);
+final Ataque punoBala= Ataque("Bug","Puño Bala",40);
+final Ataque danzaEspada= Ataque("Bug","Danza Espada",20);//mayor probabilidad estado
+final Ataque zumbido= Ataque("Bug","Zumbido",90);
+final Ataque danzaAleteo= Ataque("Bug","Danza Aleteo",20);//mayor probabilidad estado
+final Ataque vendavalB= Ataque("Bug","Vendaval",110);
+final Ataque tijeraX= Ataque("Bug","Tijera X",80);
+final Ataque megacuerno= Ataque("Bug","Megacuerno",120);
+final Ataque bocajarro= Ataque("Bug","Bocajarro",110);
+
+
+//Psycho
+final Ataque psiquico= Ataque("Psycho","Psíquico",90);
+final Ataque confusion= Ataque("Psycho","Confusión",50);
+final Ataque psicoCorte= Ataque("Psycho","Psico Corte",70);
+final Ataque ondaMental= Ataque("Psycho","Onda Mental",90);
+final Ataque psicorrayo= Ataque("Psycho","Psicorrayo",65);
+final Ataque paranormal= Ataque("Psycho","Paranormal",65);
+
+
+//Flying
+final Ataque tornado= Ataque("Flying","Tornado",40);
+final Ataque ataqueAla= Ataque("Flying","Ataque Ala",60);
+final Ataque pajaroOsado= Ataque("Flying","Pájaro Osado",120);
+final Ataque alaMortifera= Ataque("Flying","Ala Mortífera",80);
+final Ataque vendavalF= Ataque("Flying","Golpe Cabeza",110);
+final Ataque AireAfilado= Ataque("Flying","Aire Afilado",75);
+final Ataque danzaPluma= Ataque("Flying","Danza Pluma",20);//mayor probabilidad estado
+
+// Ground
+
+
+//Poison
+
+
+//Fight
+
+
+//Grass
+
+
+//Ice
+
+
+//electric
+
+
+//water
 final Ataque hidrobomba = Ataque("Agua", "Hidrobomba", 110);
 final Ataque surf = Ataque("Agua", "Surf", 90);
 final Ataque pistolaAgua = Ataque("Agua", "Pistola Agua", 40);
 
-//planta
-final Ataque rayoSolar = Ataque("Planta", "Rayo Solar", 120);
-final Ataque hojaAfilada = Ataque("Planta", "Hoja Afilada", 55);
-final Ataque latigoCepa = Ataque("Planta", "Látigo Cepa", 45);
+//fire
+final Ataque lanzallamas = Ataque("Fire", "Lanzallamas", 90);
+final Ataque ascuas = Ataque("Fire", "Ascuas", 40);
+final Ataque giroFuego = Ataque("Fire", "Giro Fuego", 35);
+
+
+//Normal
+final Ataque golpeNormal= Ataque("Normal","Golpe Normal",50);
+final Ataque hiperRayo= Ataque("Normal","Hiper Rayo",150);
+final Ataque cuerpoNormal= Ataque("Normal","Cuerpo Normal",70);
+
 
 // DEFINICION DE POKEMONES
 
@@ -31,9 +122,9 @@ final PokemonBatalla feraligatr = PokemonBatalla("Agua", "Feraligatr", 165, 50, 
 final PokemonBatalla swampert = PokemonBatalla("Agua", "Swampert", 180, 50, [hidrobomba, surf]);
 
 //Planta
-final PokemonBatalla venusaur = PokemonBatalla("Planta", "Venusaur", 170, 50, [rayoSolar, latigoCepa]);
-final PokemonBatalla meganium = PokemonBatalla("Planta", "Meganium", 165, 50, [hojaAfilada, rayoSolar]);
-final PokemonBatalla sceptile = PokemonBatalla("Planta", "Sceptile", 145, 50, [hojaAfilada, latigoCepa]);
+//final PokemonBatalla venusaur = PokemonBatalla("Planta", "Venusaur", 170, 50, [rayoSolar, latigoCepa]);
+//final PokemonBatalla meganium = PokemonBatalla("Planta", "Meganium", 165, 50, [hojaAfilada, rayoSolar]);
+//final PokemonBatalla sceptile = PokemonBatalla("Planta", "Sceptile", 145, 50, [hojaAfilada, latigoCepa]);
 
 // DEFINICION DE ITEMS
 class Pocion extends Item {
@@ -81,7 +172,6 @@ class CuraEstado extends Item {
   }
 }
 
-// Ejemplos de instancias
 final Pocion pocion = Pocion("Poción", 5, 20);
 final CuraEstado despertar = CuraEstado("Despertar", 3, "Cura Congelado/Paralizado", [
   EstadoCondicion.congelado,
