cfc9c8f63ef5d375c8eb48b16cd06fc5e6951d93
Cit <sbernaldelao@gmail.com>
2025-11-26 23:06:36 -0600

    Mapa efectividad terminado

--- Proyecto3/POKEMON/Base.dart
+++ Proyecto3/POKEMON/Base.dart
@@ -46,7 +46,162 @@ class Efectividad {
   double calcularEfectividad(String tipoAtaque, String tipoEnemigo) {
     double factor = 1.0;
 
-    //Tabla de efectividad (Mapa)
+    const Map<String, Map<String, double>> efectividad = {
+  // Ataque | Defensa: {
+  'Normal': {
+    'Fight':2.0,
+    'Ghost':0.0,
+  },
+  'Fire': {
+    'Fire':0.5,
+    'Grass':0.5,
+    'Water':2.0,
+    'Rock':2.0,
+    'Bug':0.5,
+    'Ice':0.5,
+    'Steel':0.5,
+    'Ground':2.0,
+  },
+  'Water': {
+    'Fire':0.5,
+    'Water':0.5,
+    'Grass':2.0,
+    'Ice':0.5,
+    'Steel':0.5,
+    'Electric':2.0
+  },
+  'Electric': {
+    'Electric':0.5,
+    'Ground':2.0,
+    'Flying':0.5,
+    'Steel':0.5,
+  },
+  'Grass': {
+    'Fire':2.0,
+    'Water':0.5,
+    'Electric':0.5,
+    'Grass':0.5,
+    'Ice':2.0,
+    'Poison':2.0,
+    'Ground':0.5,
+    'Flying':2.0,
+    'Bug':2.0,
+  },
+  'Ice': {
+    'Fire':2.0,
+    'Ice':0.5,
+    'Fight':2.0,
+    'Rock':2.0,
+    'Steel':2.0,
+  },
+  'Fight': {
+    'Flying':2.0,
+    'Psycho':2.0,
+    'Bug':0.5,
+    'Rock':0.5,
+    'Dark':0.5,
+  },
+  'Poison': {
+    'Grass':0.5,
+    'Fight':0.5,
+    'Poison':0.5,
+    'Ground':2.0,
+    'Psycho':2.0,
+    'Bug':0.5,
+  },
+  'Ground': {
+    'Water':2.0,
+    'Electric':0.0,
+    'Grass':2.0,
+    'Ice':2.0,
+    'Poison':0.5,
+    'Rock':0.5,
+  },
+  'Flying': {
+    'Electric':2.0,
+    'Grass':0.5,
+    'Ice':2.0,
+    'Fight':0.5,
+    'Ground':0.0,
+    'Bug':0.5,
+    'Rock':2.0,
+  },
+  'Psycho': {
+    'Fight':0.5,
+    'Psycho':0.5,
+    'Bug':2.0,
+    'Ghost':2.0,
+    'Dark':2.0,
+  },
+  'Bug': {
+    'Fire':2.0,
+    'Grass':0.5,
+    'Fight':0.5,
+    'Ground':0.5,
+    'Flying':2.0,
+    'Rock':2.0,
+  },
+  'Rock': {
+    'Normal':0.5,
+    'Fire':0.5,
+    'Water':2.0,
+    'Grass':2.0,
+    'Fight':2.0,
+    'Poison':0.5,
+    'Ground':2.0,
+    'Flying':0.5,
+    'Steel':2.0,
+  },
+  'Ghost': {
+    'Normal':0.0,
+    'Fight':0.0,
+    'Poison':0.5,
+    'Bug':0.5,
+    'Ghost':2.0,
+    'Dark':2.0,
+  },
+  'Dragon': {
+    'Fire':0.5,
+    'Water':0.5,
+    'Electric':0.5,
+    'Grass':0.5,
+    'Ice':2.0,
+    'Dragon':2.0,
+  },
+  'Dark': {
+    'Fight':2.0,
+    'Psycho':0.0,
+    'Bug':2.0,
+    'Ghost':0.5,
+    'Dark':0.5,
+  },
+  'Steel': {
+    'Normal':0.5,
+    'Fire':2.0,
+    'Grass':0.5,
+    'Ice':0.5,
+    'Fight':2.0,
+    'Poison':0.0,
+    'Ground':2.0,
+    'Flying':0.5,
+    'Psycho':0.5,
+    'Bug':0.5,
+    'Rock':0.5,
+    'Ghost':0.5,
+    'Dragon':0.5,
+    'Dark':0.5,
+    'Steel':0.5,
+  },
+
+    
+};
+
+    if (efectividad.containsKey(tipoAtaque)) {
+      final ataques = efectividad[tipoAtaque]!;
+      if (ataques.containsKey(tipoEnemigo)) {
+        factor = ataques[tipoEnemigo]!;
+      }
+    }
 
     return factor;
   }
